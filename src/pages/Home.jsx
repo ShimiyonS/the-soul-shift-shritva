2407ccc39e970bdadbf658d3b74ef29ff8115b8b
ShimiyonS <shimjnss@gmail.com>
2025-08-26 13:14:19 +0530

    heading changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -90,9 +90,7 @@ export const Home = () => {
             <Header
 
                 subtitle="Powered by Intent Shūnyaivāham"
-
                 title="Being Your True Essence"
-
                 description="Where healing is not becoming more, but remembering who you truly are. Step into a sacred space of stillness, soul remembrance, and deep energetic transformation."
                 buttons={[
                     { label: "Begin Your Journey", link: "/services" },
